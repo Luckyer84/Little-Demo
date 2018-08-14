cdb6693115415043d1018c9322ca70ddf38a388c
Luckyer84 <462281484@qq.com>
2018-08-14 13:36:26 +0800

    demo

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 			dataType:"json",
 			success:function(data){
 				if(data.code==1){
-			    alert("用户名异步获取成功！");
+			  /*  alert("用户名异步获取成功！");*/
 					data.list.forEach(function(item){
 						if(item.name=="McGrade"){
 							$(".showInfo").html(item.name)
@@ -70,33 +70,42 @@ $(document).ready(function() {
    
    //图片切换
   	let count=0;
+  	const fig=$(".roll figure");//获取图像的父移动框
+   	const blackPoint=$("#main-blackPoint li"); //黑点
    	const move=function(){
-   	const fig=$(".roll figure");
-   let nun = parseInt(fig.position().left)/-650;//获取鼠标事件当前图片的位置
-   let count =nun;
-	count++;
-	const wid=-650*count;
-	fig.animate({left:wid},100);
-		
-	if(count>3){
-		count=0;
-		fig.animate({left:0},0);
-	}
-    };
-    let timer=setInterval(move,4000);
+				let nun = parseInt(fig.position().left)/-650;//获取鼠标事件当前图片的位置
+				let count =nun;
+				count++;
+				const wid=-650*count;
+				fig.animate({left:wid},0);	
+				if(count>4){//少一张
+					count=0;
+					fig.animate({left:0},0);
+					}
+				
+				 var $blackI =$(blackPoint[count]);
+			   	 $blackI.css("background-color","white");
+			     blackPoint.not($blackI).css("background-color","rgba(255,255,255,0.2)");
+	};
+    
+    let timer=setInterval(move,3000);
 
-   var fig=$(".roll figure");//容器
+ /*  var fig=$(".roll figure");//容器*/
     const pic=$(".roll img");//图片
-    const blackPoint=$("#main-blackPoint li"); //黑点
+  /*  const blackPoint=$("#main-blackPoint li"); //黑点*/
     for(let i=0;i<blackPoint.length;i++){
 	 const $blackPoint_i = $(blackPoint[i]); 
     $blackPoint_i.hover(function(){
+    	
     	clearInterval(timer);
+    
     	const shift=-pic.width()*i;
-    	fig.animate({left:shift},500) 	
+    	$blackPoint_i.css("background-color","white");
+    	blackPoint.not($blackPoint_i).css("background-color","rgba(255,255,255,0.2)");
+    	fig.animate({left:shift},0) 	
 		  },function(){
-			clearInterval(timer);
-			timer = setInterval(move,4000);
+			timer = setInterval(move,3000);
+			$blackPoint_i.css("background-color","white");
 		  })
 	}; 	
    //头像下面栏目事件的监听
